1cfcc69c33bb344a4f9274f4b4746a81e1ca59dd
Ryan Smulan <ryan.smulan@gmail.com>
2015-09-21 00:16:54 -0400

    Adding configuration utility method

---
+++ src/NLog.Raygun/NLogRayGunLogger.cs
@@ -0,0 +1,29 @@
+﻿using NLog.Config;
+
+namespace NLog.Raygun
+{
+    /// <summary>
+    /// Utility class used to initialize the RayGun NLog Target
+    /// </summary>
+    public static class NLogRayGunLogger
+    {
+        private static readonly object Lock = new object();
+        private static bool _initialized;
+
+        public static void Initialize()
+        {
+            if (!_initialized)
+            {
+                lock (Lock)
+                {
+                    if (!_initialized)
+                    {
+                        ConfigurationItemFactory.Default.Targets.RegisterDefinition("RayGun", typeof (RayGunTarget));
+                        LogManager.ReconfigExistingLoggers();
+                        _initialized = true;
+                    }
+                }
+            }
+        }
+    }
+}
